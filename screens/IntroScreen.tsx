48377ffcd26bcb4673c465a2a1b53031134eb36d
Shahzaib <shahzaib.m@digiryte.com>
2021-05-09 21:52:48 +0530

    intro screen first section completed

--- screens/IntroScreen.tsx
+++ screens/IntroScreen.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { StyleSheet, View, ScrollView } from "react-native";
+import { StyleSheet, View } from "react-native";
 import CustomButton from "../components/UI/CustomButton";
 import WelcomScreenCard from "../components/WelcomScreenCard";
 
 const IntroScreen = () => {
   return (
-    <ScrollView>
-      <View style={styles.screen}>
+    <View style={styles.screen}>
+      <View style={{ marginTop: "auto" }}>
         <WelcomScreenCard
           imgPath={require("../assets/stepOne.png")}
           heading={`Learn anytime \n and anywhere`}
           subheading={`Quarantine is the perfect time to spend your '\n'  day learning something new, from anywhere!`}
         />
 
-        <WelcomScreenCard
+        {/* <WelcomScreenCard
           imgPath={require("../assets/stepTwo.png")}
           heading={`Find a course \n for you`}
           subheading={`Quarantine is the perfect time to spend your \n day learning something new, from anywhere!`}
@@ -23,11 +23,13 @@ const IntroScreen = () => {
           imgPath={require("../assets/stepThree.png")}
           heading={`Improve your skills`}
           subheading={`Quarantine is the perfect time to spend your '\n'  day learning something new, from anywhere!`}
-        />
+        /> */}
+      </View>
 
+      <View style={{ marginTop: "auto", marginBottom: 30 }}>
         <CustomButton variant="buttonMediumText">Next</CustomButton>
       </View>
-    </ScrollView>
+    </View>
   );
 };
 
@@ -39,6 +41,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    paddingBottom: 30
   },
 });
