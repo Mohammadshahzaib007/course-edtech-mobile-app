3dd4eec3db60690ab3391fb2783fd31e2119fb90
Shahzaib <shahzaib.m@digiryte.com>
2021-05-09 16:44:12 +0530

    added custom fonts

--- App.tsx
+++ App.tsx
@@ -1,13 +1,46 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
+import "react-native-gesture-handler";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import IntroScreen from "./screens/IntroScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+const fetchFonts = async () => {
+  Font.loadAsync({
+    "rubik-regular": require("./assets/fonts/Rubik-Regular.ttf"),
+    "rubik-medium": require("./assets/fonts/Rubik-Medium.ttf"),
+    "rubik-bold": require("./assets/fonts/Rubik-Bold.ttf"),
+  });
+};
+
+const stack = createStackNavigator();
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={() => setFontLoaded(false)}
+      />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello Course EduTech App.</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <stack.Navigator>
+        <stack.Screen
+          name="Intro"
+          options={{ title: "Skip" }}
+          component={IntroScreen}
+        />
+      </stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/IntroScreen.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const IntroScreen = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 40, fontFamily: "rubik-bold" }}>
+        I am Intro Screen
+      </Text>
+    </View>
+  );
+};
+
+export default IntroScreen;
+
+const styles = StyleSheet.create({});
