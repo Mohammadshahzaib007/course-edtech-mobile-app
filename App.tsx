67a507004bdd205e53d257f7b83f54708ab08cce
Shahzaib <shahzaib.m@digiryte.com>
2021-05-09 15:19:37 +0530

    testing

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Course EduTech App</Text>
+      <Text>Hello Course EduTech App.</Text>
       <StatusBar style="auto" />
     </View>
   );
