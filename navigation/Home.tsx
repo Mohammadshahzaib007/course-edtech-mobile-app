6d91ca975cd5e936225af3991c161c90facc008a
Shahzaib <shahzaib.m@digiryte.com>
2021-05-22 17:54:52 +0530

    tab navigation completed

--- navigation/Home.tsx
+++ navigation/Home.tsx
@@ -6,35 +6,77 @@ import ProfileScreen from "../screens/ProfileScreen";
 import YourCoursesScreen from "../screens/YourCoursesScreen";
 import SettingScreen from "../screens/SettingScreen";
 import { createStackNavigator } from "@react-navigation/stack";
+import colors from "../constants/colors";
+import { Foundation } from "@expo/vector-icons";
+import { Octicons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 const HomeStack = createStackNavigator();
 
 const Home = () => {
   return (
-    <Tab.Navigator tabBarOptions={{tabStyle: {
-        alignItems: 'center',
-        justifyContent: 'center'
-        
-    }}}>
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: colors.primary,
+        inactiveTintColor: "#BEBAB3",
+        style: {
+          borderTopLeftRadius: 16,
+          borderTopRightRadius: 16,
+          borderColor: "#BEBAB3",
+          borderWidth: 1,
+          height: 98,
+          elevation: 0,
+        },
+        tabStyle: {
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#fff",
+          overflow: "hidden",
+          borderTopRightRadius: 16,
+          borderTopLeftRadius: 16,
+          paddingVertical: 25,
+        },
+      }}
+    >
       <Tab.Screen
         name="Home"
-        options={{ title: "Home" }}
+        options={{
+          title: "Home",
+          tabBarIcon: ({ size, color }) => (
+            <Foundation name="home" size={size} color={color} />
+          ),
+        }}
         component={HomeScreen}
       />
       <Tab.Screen
         name="Profile"
-        options={{ title: "Profile" }}
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ size, color }) => (
+            <Octicons name="person" size={size} color={color} />
+          ),
+        }}
         component={ProfileScreen}
       />
       <Tab.Screen
         name="YourCourses"
-        options={{ title: "Your courses" }}
+        options={{
+          title: "Your courses",
+          tabBarIcon: ({ size, color }) => (
+            <Foundation name="book-bookmark" size={size} color={color} />
+          ),
+        }}
         component={YourCoursesScreen}
       />
       <Tab.Screen
         name="Settings"
-        options={{ title: "Settings" }}
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings-sharp" size={size} color={color} />
+          ),
+        }}
         component={SettingScreen}
       />
     </Tab.Navigator>
