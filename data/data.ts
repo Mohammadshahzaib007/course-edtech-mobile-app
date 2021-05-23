00279bb58a70afb7f4906e7ea16c6fde67c99819
Shahzaib <shahzaib.m@digiryte.com>
2021-05-23 16:54:16 +0530

    updated data

--- data/data.ts
+++ data/data.ts
@@ -9,7 +9,8 @@ export const courses: Course[] = [
     thumbnailUrl:
       "https://img-c.udemycdn.com/course/240x135/1033446_4080_2.jpg?Expires=1621840086&Signature=HjqAPuith6eHAOE3IOLzRThH6CCoBfSab~gFKCaCbgYQ7BXjgLiUzGqftg49DZ5JwJDY7fWNwXEIzoHzZf6d90BamdVb6rSQGBPmq7qp3hpqvZAgFqvXE6ksNjgZXFMIh3Amu1KT0PwtJriG4pLngm3K3A6bs79mO91JJIt1NF2v9Uk-V1hyO5F9aPySKWkRU-iEj1FonUlKfZTJr~2HAbZcgpDVcPHrb4oipIGLSJBJg3~Ej0heK8G~e65bd0PYwLyY30iW3hPaimoDfeDzpXr5Nf2uwl6egGYd1dd-nnzznKvmoJx04OiDRiWcOIYlODOyVDnZvFnup~QY7SC1ng__&Key-Pair-Id=APKAITJV77WS5ZT7262A",
     price: "$ 50",
-    aboutTheCourse: "This is an amazing course. But it now",
+    aboutTheCourse:
+      "When it comes to User Interface (UI) design, your job — whether you're a designer, developer, UXer or a mix of all those things — is to make sure that no aspect of someone’s onscreen interaction happens without explicit intent. The UI design choices we make have to reflect the user’s motivations, expectations, environment and possible actions.",
     categiroy: ["ui", "ux"],
   },
 
@@ -21,7 +22,8 @@ export const courses: Course[] = [
     thumbnailUrl:
       "https://img-c.udemycdn.com/course/240x135/3574565_570d.jpg?Expires=1621756360&Signature=E1kZQJCi3HPSn2XEnm-0Kuw9EtP7wQ0nCOugF06b~TxSn~h4ABC9scVXEr4bnZs4E48PBS1nsa3xv8mRBE6KRsh7gpXbs1CZlatENfcH3wrp1VFAsKjmzKQOISixEq3ghgzCaMpnz5Mu1Zj9BfNK7GrE~OUeWhDDpziAi6ME-MATlMerwMNSRyKraZDguazX6pG3yCs1kxwALhbe0Y03jPyqslhsl9y-tkRIyo9jhCKjOc9HfmuppsG8ZyU~upyAyZ~ELNf058PUoTdXSk7yQgedD8vM5ejtb8NpzNSgY6REspfI-kzRrpncNh373KAZhhbh7HvsmsgLezP-yswo~g__&Key-Pair-Id=APKAITJV77WS5ZT7262A",
     price: "$ 65",
-    aboutTheCourse: "This is an amazing course. But it now",
+    aboutTheCourse:
+      "Advanced framework development with Coded UI Test 2015/2017 course is designed in such a way that anyone who takes this course will understand the ins and outs of Visual studio Coded UI like never before. Coded UI  testing is always known for its complexity in coding among many testers and developers, but at the end of this course one will know that the theory is totally wrong !!!",
     categiroy: ["ui", "ux"],
   },
 
@@ -46,7 +48,8 @@ export const courses: Course[] = [
     thumbnailUrl:
       "https://img-b.udemycdn.com/course/240x135/892102_963b.jpg?secure=vyNtf_Di0zIogjfT8n3PIg%3D%3D%2C1621840047",
     price: "$ 99",
-    aboutTheCourse: "This is an amazing course. But it now",
+    aboutTheCourse:
+      "Welcome to the Complete iOS App Development Bootcamp. With over 17,000 ⭐️⭐️⭐️⭐️⭐️ ratings, this is the HIGHEST RATED iOS Course of all time!  This Swift 4 course is based on our in-person app development bootcamp in London. We've perfected the curriculum over 3 years of in-person teaching.  Our complete app development bootcamp teaches you how to code using Swift 4 and build beautiful iOS 11 apps for iPhone and iPad. Even if you have ZERO programming experience.",
     categiroy: ["swift"],
   },
 
@@ -58,7 +61,8 @@ export const courses: Course[] = [
     thumbnailUrl:
       "https://img-c.udemycdn.com/course/240x135/2002862_aa45_5.jpg?Expires=1621754175&Signature=OmW8ichSsv7Fk3~0bBSH-mPbYAvc53M0OQiIkotTlTpO~D7EKwLr~DmitzSB8qoT8irGWiTzOrjMF5qAiLhvcFvwMsw2B5dWjMybj92d8-8RrxlO3lXi~Xt5QoRXcblH4bKnyo4bsynMCwiDCLZe1p91htqgy1vpWLqN6RYOzFpcA9j2DsBKtzTB2ljlVqwfVoG2LjLpEDfCDMUPUQH~HyjYYEI8pR6kR3JFA5lFIpeG0xSrRhQgagqNzeuSX5uEttY6kbA8mMvTPKfpLGs7rcaHgHnB3~0oYuUGEs8ux8b6hZtvFvvH0lOpRWNebUvOzgY9aqgA-URli5XmXnczCA__&Key-Pair-Id=APKAITJV77WS5ZT7262A 1x, https://img-c.udemycdn.com/course/480x270/2002862_aa45_5.jpg?Expires=1621754175&Signature=OmW8ichSsv7Fk3~0bBSH-mPbYAvc53M0OQiIkotTlTpO~D7EKwLr~DmitzSB8qoT8irGWiTzOrjMF5qAiLhvcFvwMsw2B5dWjMybj92d8-8RrxlO3lXi~Xt5QoRXcblH4bKnyo4bsynMCwiDCLZe1p91htqgy1vpWLqN6RYOzFpcA9j2DsBKtzTB2ljlVqwfVoG2LjLpEDfCDMUPUQH~HyjYYEI8pR6kR3JFA5lFIpeG0xSrRhQgagqNzeuSX5uEttY6kbA8mMvTPKfpLGs7rcaHgHnB3~0oYuUGEs8ux8b6hZtvFvvH0lOpRWNebUvOzgY9aqgA-URli5XmXnczCA__&Key-Pair-Id=APKAITJV77WS5ZT7262A 2x",
     price: "$ 75",
-    aboutTheCourse: "This is an amazing course. But it now",
+    aboutTheCourse:
+      "Make yourself a more valuable member of the team by learning how to help organizations make changes faster and with less expense by using Agile approaches. In nature, as in business – you adapt, or you die…  And with over 70% of companies using Agile for their projects, not knowing the fundamental concepts, principles, and values just isn’t an option anymore.",
     categiroy: ["management"],
   },
 ];
